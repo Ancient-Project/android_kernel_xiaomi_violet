fiqritest/android_kernel_ginkgo_backup

net: wireguard: Use totalram_pages() for linux v4.14
Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

net/wireguard/src/compat/compat.h
@@ -757,7 +757,7 @@ static inline void crypto_xor_cpy(u8 *dst, const u8 *src1, const u8 *src2,
 #define hlist_add_behind(a, b) hlist_add_after(b, a)
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0) && !defined(ISCENTOS8S)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0) && !defined(ISCENTOS8S)
 #define totalram_pages() totalram_pages
 #endif
 

Ancient-Project/android_kernel_xiaomi_violet

Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

net/wireguard/src/compat/compat.h
@@ -777,7 +777,7 @@ static inline void crypto_xor_cpy(u8 *dst, const u8 *src1, const u8 *src2,
 #define hlist_add_behind(a, b) hlist_add_after(b, a)
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0) && !defined(ISRHEL8)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0) && !defined(ISRHEL8)
 #define totalram_pages() totalram_pages
 #endif
 