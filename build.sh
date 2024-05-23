e86a7d42611c4ab973e47ce57c3397066e197d3f
rezaadi0105 <rezaadipangestu5@gmail.com>
2024-05-23 14:14:31 +0700

    test

--- build.sh
+++ build.sh
@@ -55,7 +55,7 @@ if [ -d "/root/project" ]; then
 	git clone --depth=1 https://github.com/fabianonline/telegram.sh.git telegram
 	# Set environment for telegram
 	export TELEGRAM_DIR="$KERNEL_DIR/telegram/telegram"
-	export TELEGRAM_CHAT="-1002143086681"
+	export TELEGRAM_CHAT="-1001994137398"
 	# Get CPU name
 	export CPU_NAME="$(lscpu | sed -nr '/Model name/ s/.*:\s*(.*) */\1/p')"
 else
