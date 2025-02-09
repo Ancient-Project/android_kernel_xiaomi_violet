315f226c73b776bb42eca60b67a1d6d9221242bf
rezaadi0105 <rezaadipangestu5@gmail.com>
2025-02-10 00:25:54 +0700

    build: track kernelsu next
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- build.sh
+++ build.sh
@@ -93,7 +93,7 @@ tg_post_build() {
 
 # Set function for setup KernelSU
 setup_ksu() {
-	curl -kLSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+	curl -kLSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s next
 	if [ -d "$KERNEL_DIR"/KernelSU ]; then
 		git apply KernelSU-hook.patch
 	else
